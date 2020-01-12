efedefcf547dd3740c5730d7c158c6d63b726ad4
Alexander Inca <alex.inca66@gmail.com>
2020-01-12 09:42:51 -0500

    ainca - Modificacion a la pantalla de torneos

--- app/Styles/styles.js
+++ app/Styles/styles.js
@@ -1,47 +1,48 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-          flex: 1,
-         // justifyContent: 'center',
-          backgroundColor: '#ebebeb'
-      },
+   container: {
+      flex: 1,
+      // justifyContent: 'center',
+      backgroundColor: '#ebebeb',
+   },
 
-      gridView: {
-        marginTop: 20,
-        flex: 1,
-      },
-      itemContainer: {
-       // justifyContent: 'center',
-        padding: 5,
-        height: 150,
-      },
-      itemName: {
-        fontSize: 16,
-        color: '#e67e22',
-        fontWeight: '600', 
-        //alignSelf: 'flex-start',
-        //justifyContent: 'flex-end',   
-      },
-      itemYear: {
-        fontWeight: '600',
-        fontSize: 12,
-        color: '#e67e22',
-        //justifyContent: 'flex-end',
-        //position:'relative',
-
-      },
-      image:{
-        height: '100%',
-        width: '100%',
-       // alignItems:'flex-start',
+   gridView: {
+      marginTop: 20,
+      flex: 1,
+   },
+   itemContainer: {
+      // justifyContent: 'center',
+      padding: 5,
+      height: 150,
+   },
+   itemName: {
+      fontSize: 16,
+      color: '#e67e22',
+      fontWeight: '600',
+      //alignSelf: 'flex-start',
+      //justifyContent: 'flex-end',
+   },
+   itemYear: {
+      fontWeight: '600',
+      fontSize: 12,
+      color: '#e67e22',
+      //justifyContent: 'flex-end',
+      //position:'relative',
+   },
+   image: {
+      height: 150,
+      width: 150,
+      // alignItems:'flex-start',
       // justifyContent: "space-evenly",
-        borderRadius: 5,
-      },
-      iconFav:{
-        alignSelf: 'flex-end',
-        justifyContent: 'flex-start',
-      }
-  });
+      borderRadius: 5,
+      padding: 0,
+      margin: 0,
+   },
+   iconFav: {
+      alignSelf: 'flex-end',
+      justifyContent: 'flex-start',
+   },
+});
 
 export default styles;
--- app/components/ItemTorneos.js
+++ app/components/ItemTorneos.js
@@ -1,75 +1,91 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, ImageBackground, TouchableOpacity,Alert } from 'react-native';
+import {
+   StyleSheet,
+   View,
+   Text,
+   ImageBackground,
+   TouchableOpacity,
+   Alert,
+} from 'react-native';
+import { Image } from 'react-native-elements';
 import { FlatGrid } from 'react-native-super-grid';
-import { IconButton} from 'react-native-paper';
-import styles from "../Styles/styles";
+import { IconButton } from 'react-native-paper';
+import styles from '../Styles/styles';
 export default class Example extends Component {
-  state ={
-    colorFavorito:"#ffffff"
-  };
+   state = {
+      colorFavorito: '#ffffff',
+   };
 
- addFavorito=(torneo)=>{
-     if(torneo.favorito){
-       global.itemsRef.child(torneo.id).update({favorito:false});
-       this.setState({colorFavorito:"#ffffff"});
-    	}else{
-        global.itemsRef.child(torneo.id).update({favorito:true});
-        this.setState({colorFavorito:"#F79405"});
-        }
-  }
+   addFavorito = torneo => {
+      if (torneo.favorito) {
+         global.itemsRef.child(torneo.id).update({ favorito: false });
+         this.setState({ colorFavorito: '#ffffff' });
+      } else {
+         global.itemsRef.child(torneo.id).update({ favorito: true });
+         this.setState({ colorFavorito: '#F79405' });
+      }
+   };
 
-  imagePressed(item){
-    global.idTorneo = item.id;
-    
-    console.log('torneo elegido ',item);
-	let ruta="TabEquipos"
-	if(item.estado=='A'){
-		ruta="PerfilTorneo"
-	}else{
-		global.listaCategorias = Object.getOwnPropertyNames(item.categorias);
-	}
-     this.props.nav.navigate(ruta,{nombreTorneo:item.nombreTorneo})
-  }
+   imagePressed(item) {
+      global.idTorneo = item.id;
 
-  pintarFavorito=(favorito)=>{
-    if(favorito){
-       return "#F79405"
-    }else{
-     return "#ffffff"
-    }
-  }
+      console.log('torneo elegido ', item);
+      let ruta = 'TabEquipos';
+      if (item.estado == 'A') {
+         ruta = 'PerfilTorneo';
+      } else {
+         global.listaCategorias = Object.getOwnPropertyNames(item.categorias);
+      }
+      this.props.nav.navigate(ruta, { nombreTorneo: item.nombreTorneo });
+   }
 
+   pintarFavorito = favorito => {
+      if (favorito) {
+         return '#F79405';
+      } else {
+         return '#ffffff';
+      }
+   };
 
-  render() {	  
-    return (
-      <FlatGrid
-        itemDimension={130}
-        items={this.props.torneos}
-        style={styles.gridView}
-        renderItem={({ item, index }) => (	
-		
-          <View style={[styles.itemContainer]} >
-            <TouchableOpacity onPress={()=>this.imagePressed(item)}>
-		        	<ImageBackground  source = {{uri: item.imagenTorneo}}
-								                style = {styles.image}>
-               
-					        <IconButton
-					          	icon="star"
-					          	color={this.pintarFavorito(item.favorito)}
-					          	size={25}
-						        align= "left"
-						        onPress={() => this.addFavorito(item)}
-                    style={styles.iconFav}
-				        	/>
-                 
-          	                <Text style={styles.itemName}>{item.nombreTorneo}</Text>
-					        <Text style={styles.itemYear}>{item.anio}</Text>
-		        </ImageBackground>  
-              
-            </TouchableOpacity>
-            </View>
-        )}
-      />
-    );
-  }
+   render() {
+      return (
+         <FlatGrid
+            itemDimension={150}
+            fixed={true}
+            items={this.props.torneos}
+            style={styles.gridView}
+            renderItem={({ item, index }) => (
+               <View style={[styles.itemContainer, border('#7A7A7A')]}>
+                  <TouchableOpacity
+                     style={border('#0000')}
+                     onPress={() => this.imagePressed(item)}
+                  >
+                     <ImageBackground
+                        source={{ uri: item.imagenTorneo }}
+                        style={styles.image}
+                     >
+                        <IconButton
+                           icon="star"
+                           color={this.pintarFavorito(item.favorito)}
+                           size={25}
+                           align="left"
+                           onPress={() => this.addFavorito(item)}
+                           style={styles.iconFav}
+                        />
+                     </ImageBackground>
+
+                     <View style={border('#ffffff')}>
+                        <Text style={styles.itemName}>{item.nombreTorneo}</Text>
+                        <Text style={styles.itemYear}>{item.anio}</Text>
+                     </View>
+                  </TouchableOpacity>
+               </View>
+            )}
+         />
+      );
+   }
 }
+
+const border = color => {
+   return { borderColor: color, borderWidth: 2 };
+};
