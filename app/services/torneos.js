a08a9fdeb9a1eea307a15d18696f1fdfb793ab05
Santiago Mosquera <zantycb89@gmail.com>
2020-02-01 19:21:27 -0500

    torneos

--- app/services/torneos.js
+++ app/services/torneos.js
@@ -1,11 +1,51 @@
 import { firebase } from '@react-native-firebase/database';
 import { crearPermiso } from './permisos.js';
 
-export const cargarTorneos = object => {
-   console.log('ingresa a cargar');
-   const itemsRef = firebase.database().ref('torneos');
-   listenForItems(itemsRef, object);
+const torneosEnCurso = [];
+const torneosPorIniciar = [];
+const torneosIniciados = [];
+
+let itemsRef = null;
+export const cargarTorneosEnCurso = repintar => {
+   console.log('ingresa a cargar ');
+   if (itemsRef == null) {
+      itemsRef = firebase.database().ref('torneos');
+   }
+   itemsRef
+      .orderByChild('estado')
+      .equalTo('A')
+      .on('child_added', snap => {
+         torneosEnCurso.push(snap.val());
+         repintar(torneosEnCurso);
+      });
 };
+export const cargarTorneosFinalizados = repintar => {
+   console.log('ingresa a cargar ');
+   if (itemsRef == null) {
+      itemsRef = firebase.database().ref('torneos');
+   }
+   itemsRef
+      .orderByChild('estado')
+      .equalTo('F')
+      .on('child_added', snap => {
+         torneosPorIniciar.push(snap.val());
+         repintar(torneosPorIniciar);
+      });
+};
+export const cargarTorneosPorIniciar = repintar => {
+   console.log('ingresa a cargar ');
+   if (itemsRef == null) {
+      itemsRef = firebase.database().ref('torneos');
+   }
+   itemsRef
+      .orderByChild('estado')
+      .equalTo('C')
+      .on('child_added', snap => {
+         torneosIniciados.push(snap.val());
+         repintar(torneosIniciados);
+      });
+};
+
 export const recuperarTorneo = fn => {
    var torneoRef = firebase.database().ref('torneos/' + global.idTorneo);
    torneoRef.on('value', snap => {
@@ -22,53 +62,6 @@ export const guardarTorneo = torneo => {
    itemsRef.set(torneo);
 
    crearPermiso(torneo.correoOrganizador, 'torneos', id_torneo);
-};
-
-const listenForItems = (itemsRef, object) => {
-   itemsRef.on('value', snap => {
-      snap.forEach(child => {
-         if (buscarTorneo(child.val().id) == null) {
-            global.torneos.push(child.val());
-         } else {
-            reemplazarTorneo(child.val());
-         }
-      });
-      object.setState({
-         listaTorneos: global.torneos,
-      });
-
-      if (global.enCursoComponent != null) {
-         cargarTorneo('C', enCursoComponent, 1);
-      }
-
-      if (global.porIniciarComponent != null) {
-         cargarTorneo('I', porIniciarComponent, 1);
-      }
-
-      if (global.favoritosComponent != null) {
-         cargarTorneo(true, favoritosComponent, 2);
-      }
-   });
-};
-
-const cargarTorneo = (estado, componente, modo) => {
-   var lista = [];
-   for (var i = 0; i < global.torneos.length; i++) {
-      if (modo == 1) {
-         if (global.torneos[i].estado == estado) {
-            lista.push(global.torneos[i]);
-         }
-      } else if (modo == 2) {
-         if (global.torneos[i].favorito == estado) {
-            lista.push(global.torneos[i]);
-         } else {
-            if (lista.length > 0) {
-               lista.splice(i, 1);
-            }
-         }
-      }
-   }
-   componente.setState({ listaTorneos: lista });
 };
 
 const buscarTorneo = idTorneo => {
